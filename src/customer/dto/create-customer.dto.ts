6147052c9f810a384d03ad8d67f57e94309f8586
LeonMesquita <leonmesquita16@gmail.com>
2023-02-01 02:09:25 -0300

    feat: create a new customer

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,1 +1,18 @@
-export class CreateCustomerDto {}
+import { Customer } from '../entities/customer.entity';
+import { IsDate, IsString, Matches } from 'class-validator';
+
+export class CreateCustomerDto extends Customer {
+  @IsString()
+  name: string;
+
+  @IsString()
+  cpf: string;
+
+  @Matches(
+    /^(0[1-9]|[12][0-9]|3[01])[\/|-](0[1-9]|1[012])[\/|-](19|20)\d\d$/i,
+    {
+      message: '$property must be formatted as DD-MM-YYYY or DD/MM/YYYY',
+    },
+  )
+  birth_date: string;
+}
