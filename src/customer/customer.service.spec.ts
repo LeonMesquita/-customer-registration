b925ae16f162417d3e252df63202b65beda7c7c8
LeonMesquita <leonmesquita16@gmail.com>
2023-02-01 21:23:17 -0300

    test: return the created customer

--- src/customer/customer.service.spec.ts
+++ src/customer/customer.service.spec.ts
@@ -1,12 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CustomerService } from './customer.service';
+import { CreateCustomerDto } from './dto/create-customer.dto';
+import { Customer } from './entities/customer.entity';
 
 describe('CustomerService', () => {
   let service: CustomerService;
+  const mockedCustomer: Customer = {
+    id: 1,
+    name: 'Mocked Name',
+    cpf: '834.763.564-15',
+    birth_date: new Date(),
+  };
+  const mockedCustomerDto: CreateCustomerDto = {
+    name: 'Mocked Name',
+    cpf: '834.763.564-15',
+    birth_date: '16/04/1998',
+  };
+
+  const mockedCreateCustomer = jest.fn().mockResolvedValue(mockedCustomer);
+  const mockedFindUnique = jest.fn().mockResolvedValue(null);
+
+  const prismaServiceMock = {
+    customer: {
+      create: mockedCreateCustomer,
+      findUnique: mockedFindUnique,
+    },
+  };
+
+  const customersList: Array<Customer> = [mockedCustomer];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomerService],
+      providers: [
+        CustomerService,
+        PrismaService,
+        {
+          provide: PrismaService,
+          useValue: prismaServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<CustomerService>(CustomerService);
@@ -14,5 +47,14 @@ describe('CustomerService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return the created customer', async () => {
+      const response = await service.create(mockedCustomerDto);
+      expect(mockedFindUnique).toHaveBeenCalled();
+      expect(mockedCreateCustomer).toHaveBeenCalled();
+      expect(response).toEqual(mockedCustomer);
+    });
   });
 });
